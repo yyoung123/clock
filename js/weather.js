f6d810d292dcb1b707282571767bbe67510117ae
yyoung123 <111401664+yyoung123@users.noreply.github.com>
2022-11-11 16:41:25 +0900

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -5,7 +5,6 @@ function onGeoOk(position) {
   // console.log(`You live in ${lat}, ${lon}`);
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
-  // console.log(url);
   fetch(url).then((response) =>
     response.json().then((data) => {
       const weatherBox = document.querySelector("#weather li:first-child");
@@ -18,7 +17,6 @@ function onGeoOk(position) {
       const weather = document.querySelector("#weather li:first-child span");
       const city = document.querySelector("#weather li:last-child span");
       city.innerText = data.name;
-      // console.log(data.name, data.weather[0].main);
       weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
     })
   );
